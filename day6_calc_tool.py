255fe1d7a492714a66a4e5ad574383ffe4d84d6f
moroshi-lab <okazakimoroshi@icloud.com>
2025-11-19 16:06:25 +0900

    dell

---
+++ day6_calc_tool.py
@@ -0,0 +1,11 @@
+# 数値を入力として受け取り、基本的な算術演算を行うプログラム
+a = float(input("一つ目の数字： "))
+b = float(input("二つ目の数字： "))
+
+print("足し算:", a + b)
+print("引き算:", a - b)
+print("掛け算:", a * b)
+print("割り算:", a / b)
+print("整数除算:", a // b)
+print("余り:", a % b)
+
---
+++ day6_food_list.py
@@ -0,0 +1,4 @@
+foods = ["寿司", "ラーメン", "カレー", "ピザ", "ハンバーガー"]
+
+for food in foods:
+    print("好きな食べ物 : " , food)
